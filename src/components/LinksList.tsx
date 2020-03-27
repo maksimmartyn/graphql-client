0710d006f3a652fb362f53afae12adabecdaa3c3
Max Martyn <martynmaksim.w@gmail.com>
2020-03-27 16:06:23 +0200

    Implement new link and vote subscription

--- src/components/LinksList.tsx
+++ src/components/LinksList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
 import Link, { LinkType } from './Link';
@@ -26,8 +26,87 @@ export const FEED_QUERY = gql`
   }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+  subscription {
+    newLink {
+      id
+      url
+      description
+      createdAt
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+  subscription {
+    newVote {
+      id
+      link {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+
 const LinksList = () => {
-    const { loading, error, data } = useQuery(FEED_QUERY);
+    const { loading, error, data, subscribeToMore } = useQuery(FEED_QUERY);
+
+    useEffect(() => {
+      subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData: subscription }) => {
+          if (!subscription.data) {
+            return prev
+          };
+
+          const newLink: LinkType = subscription.data.newLink;
+          const links: LinkType[] = prev.feed.links;
+          const exists = links.some((link) => link.id === newLink.id);
+          
+          if (exists) {
+            return prev;
+          }
+    
+          return Object.assign({}, prev, {
+            feed: {
+              links: [newLink, ...links],
+              count: links.length + 1,
+              __typename: prev.feed.__typename
+            }
+          })
+        }
+      });
+
+      subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION
+      });
+    });
 
     if (loading) return <div>Loading...</div>;
     if (error) return <div>Error :(</div>;
--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,9 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context'; 
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -26,8 +29,28 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
+const wsLink = new WebSocketLink({
+    uri: `ws://localhost:4000`,
+    options: {
+      reconnect: true,
+      connectionParams: {
+        authToken: localStorage.getItem(AUTH_TOKEN),
+      }
+    }
+});
+
+const link = split(
+    ({ query }) => {
+      const defenition = getMainDefinition(query);
+
+      return (defenition.kind === 'OperationDefinition') && (defenition.operation === 'subscription');
+    },
+    wsLink,
+    authLink.concat(httpLink)
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache(),
 });
 
